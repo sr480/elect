3985a07e42c52ce1ace9c9543c8988b61f7fb7d9
Снитко Д.Ю <SnitkoDY@ugpa.ru>
2019-08-01 15:19:33 +0300

    rebuild

--- renderer.js
+++ renderer.js
@@ -7,10 +7,10 @@ for(const file of fs.readdirSync('./')) {
   ul.appendChild(li);
 }
 
-const SerialPort = require('serialport')
+const SerialPort = require('serialport');
 const port = new SerialPort('COM4', {
   baudRate: 9600
-})
+});
 
-port.write('<a1>')
-port.write(Buffer.from('<a1>'))
+port.write('<a1>');
+port.write(Buffer.from('<a1>'));
