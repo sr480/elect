34d161f7e454527fcea594168d75b84544e23695
Кострюков И.Я <KostrukovIY@ugpa.ru>
2019-08-01 14:05:11 +0300

    serial port

--- renderer.js
+++ renderer.js
@@ -6,3 +6,11 @@ for(const file of fs.readdirSync('./')) {
   li.innerText = file;
   ul.appendChild(li);
 }
+
+const SerialPort = require('serialport')
+const port = new SerialPort('COM4', {
+  baudRate: 9600
+})
+
+port.write('<a1>')
+port.write(Buffer.from('<a1>'))
